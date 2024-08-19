8a9a03265e87c9765a6a9c4fc2ce22eb598baa08
서원빈 <1bins@kakao.com>
2024-08-19 14:19:10 +0900

    :lipstick: tailwind 적용

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -7,10 +7,12 @@ const cx = classNames.bind(style);
 
 const Layout = () => {
   return(
-      <main>
-        <Nav/>
-        <div className={cx('main-content')}>
-          <Outlet/>
+      <main className={cx('flex items-center justify-center')}>
+        <div className={cx('main', 'flex bg-white rounded-3xl shadow-2xl shadow-gray-400')}>
+          <Nav/>
+          <div className={cx('main-content')}>
+            <Outlet/>
+          </div>
         </div>
       </main>
   )
