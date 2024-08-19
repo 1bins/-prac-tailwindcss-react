e034067a3fe8d102a62f7c1ac3aa33a735b25f84
서원빈 <1bins@kakao.com>
2024-08-19 14:20:32 +0900

    :white_check_mark: 테스트 router 설정

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,16 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import Layout from "components/layout";
 import Home from "pages/home";
-import Task from "pages/task";
+import Overview from "pages/overview";
 
 const Router = () => {
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout/>}>
+          <Route path="*" element={<Home />}/>
           <Route path="/" element={<Home />}/>
-          <Route path="/task" element={<Task />}/>
+          <Route path="/overview" element={<Overview />}/>
         </Route>
       </Routes>
     </BrowserRouter>
